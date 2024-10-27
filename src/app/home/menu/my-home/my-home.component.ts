d80c6325ac8c68410f6941a669c5baa18ca8040b
Biswaranjan Mohapatra <bmohapatra1983@gmail.com>
2024-10-27 22:12:09 +0530

    Template Driven Form

--- src/app/home/menu/my-home/my-home.component.ts
+++ src/app/home/menu/my-home/my-home.component.ts
@@ -1,15 +1,23 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-my-home',
   standalone: true,
-  imports: [MatButtonModule,MatButtonModule,MatCardModule],
+  imports: [MatButtonModule,MatButtonModule,MatCardModule,
+    FormsModule,MatFormFieldModule,MatInputModule,CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './my-home.component.html',
   styleUrl: './my-home.component.css'
 })
 export class MyHomeComponent {
-
+  submitData:any;
+  onSubmit(myFormData:any){
+    this.submitData = myFormData.value;
+    console.log(this.submitData.name);
+  }
 }
